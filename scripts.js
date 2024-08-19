ef783aa11861d12f3a5a992db77fa4afb4b54670
TaariqLangenhoven <149932529+TaariqLangenhoven@users.noreply.github.com>
2024-08-19 20:11:21 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+//cant use addEventListener on a anchor tag thats why we use onclick instead
+const toggleMobileMenu = ()=> {
+    const menu = document.getElementById("menu")
+    menu.classList.toggle("active")
+} 
+
